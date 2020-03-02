4ceda8eea248f466223cc96bca4ebc9b5a59b8c4
Dima-dima-dima <61017310+Dima-dima-dima@users.noreply.github.com>
2020-03-02 22:21:45 +0200

    Add files via upload

---
+++ Gun.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject firePoint;
+    public float fireRate;
+    public Bullet bullet;
+
+    private float _timeToFire = 0;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time >= _timeToFire)
+        {
+            _timeToFire = Time.time + 1 / fireRate;
+            SpawnBullet();
+        }
+    }
+
+    private void SpawnBullet()
+    {
+        Instantiate(bullet.gameObject, firePoint.transform.position, Quaternion.identity).transform.rotation =
+            firePoint.transform.rotation;
+    }
+}
