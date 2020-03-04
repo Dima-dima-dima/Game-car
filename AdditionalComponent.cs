6912c57d9c61066a4ece6e33d10920b6449af6d3
Dima-dima-dima <61017310+Dima-dima-dima@users.noreply.github.com>
2020-03-04 21:52:23 +0200

    Add files via upload

---
+++ AdditionalComponent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdditionalComponent : Component
+{
+    [Range(0,1)]
+    public float percentDamageToHull;
+
+    public Hull hull;
+    
+    public override void Destroy()
+    {
+        destroyed = true;
+        GameObject destroyedObj = Instantiate(this.gameObject, transform.position, transform.rotation);
+        destroyedObj.layer = 9;
+        destroyedObj.GetComponent<MeshCollider>().isTrigger=false;
+        destroyedObj.GetComponent<AdditionalComponent>().enabled=false;
+        destroyedObj.AddComponent<Rigidbody>().mass = mass;
+        Destroy(this.gameObject);
+        Destroy(destroyedObj.gameObject, 5f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!destroyed && car.inCollision)
+        {
+            if (other.gameObject.layer == 10)
+            {
+                health -= other.gameObject.GetComponent<Bullet>().damage;
+                car.health -= other.gameObject.GetComponent<Bullet>().damage;
+            }
+            else
+            {
+                float damage = Mathf.Sqrt(hull.impulseMagnitude) * Mathf.Sqrt(energy) / 10;
+                health -= damage;
+                hull.health -= damage * percentDamageToHull;
+                car.health -= damage + damage * percentDamageToHull;
+            }
+
+            car.inCollision = false;
+            if(hull.health <= 0) hull.Destroy();
+            else
+            if (health <= 0) Destroy();
+        }
+    }
+}
---
+++ Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float damage;
+    public float speed;
+    
+    public GameObject muzzle;
+    public GameObject hit;
+
+
+    private void Start()
+    {
+        SpawnVFX(muzzle);
+        Destroy(gameObject, 4f);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += transform.forward * (speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        SpawnVFX(hit);
+        Destroy(gameObject);
+    }
+
+    private void SpawnVFX(GameObject vfxObject)
+    {
+        var vfx = Instantiate(vfxObject, transform.position, Quaternion.identity);
+        vfx.transform.forward = gameObject.transform.forward;
+        var psVfx = vfx.GetComponent<ParticleSystem>();
+        if (psVfx != null)
+        {
+            Destroy(vfx, psVfx.main.duration);
+        }
+        else
+        {
+            var psChild = vfx.transform.GetChild(0).GetComponent<ParticleSystem>();
+            Destroy(vfx, psChild.main.duration);
+        }
+    }
+}
